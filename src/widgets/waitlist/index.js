e787a54f1180e7a9f807a99451ac504fadc3bb84
bradley vangelder <r0784535@student.thomasmore.be>
2022-01-22 21:33:13 +0100

    fixing geolocation

--- src/widgets/waitlist/index.js
+++ src/widgets/waitlist/index.js
@@ -149,14 +149,14 @@ const referrer = document.referrer;
 
 document.onreadystatechange = async () => {
     if (document.readyState === 'complete') {
-        const userInfo = await fetch('http://ip-api.com/json').then(res => res.json())
+        const userInfo = await fetch('https://geolocation-db.com/json/').then(res => res.json())
 
         if(!referrer.includes('localhost') && !referrer.includes('127.0.0.1')) {
             await fetch('https://api.lancero.app/projects/addvisit', {
                 method: 'POST',
                 body: JSON.stringify({
                     referredSite: referrer,
-                    ip: userInfo.query,
+                    ip: userInfo.IPv4,
                     country: userInfo.country,
                 }),
                 headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${pk}`},
